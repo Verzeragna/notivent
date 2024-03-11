f0d066f00b46ef8f05d653574adbd0d58ba9bb89
andreevsv <andreev.s.v.1989@gmail.com>
2024-03-11 15:54:16 +0500

    get_set_nickName Added changing nickName.

---
+++ src/main/java/ru/notivent/controller/UserController.java
@@ -0,0 +1,32 @@
+package ru.notivent.controller;
+
+import static ru.notivent.util.HttpUtil.X_UUID;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.notivent.service.UserService;
+
+@RestController
+@RequestMapping("user")
+@AllArgsConstructor
+public class UserController {
+    final UserService userService;
+
+    @GetMapping("nickName")
+    public ResponseEntity<String> getNickName(@RequestHeader(X_UUID) UUID userUuid) {
+        return ResponseEntity.ok(userService.getNickName(userUuid));
+    }
+
+    @PostMapping("nickName")
+    public ResponseEntity<Void> setNickName(@RequestHeader(X_UUID) UUID userUuid, @RequestBody String userName) {
+        userService.setNickName(userUuid, userName);
+        return ResponseEntity.ok().build();
+    }
+}
