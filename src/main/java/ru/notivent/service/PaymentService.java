c95d8e248b8929c8c0932b869f2623933fad2aa6
andreevsv <andreev.s.v.1989@gmail.com>
2024-08-29 11:52:25 +0500

    Added payment working flag.

--- src/main/java/ru/notivent/service/PaymentService.java
+++ src/main/java/ru/notivent/service/PaymentService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -29,7 +30,13 @@ public class PaymentService {
   final UserService userService;
   final SubscriptionService subscriptionService;
 
+  @Value("${payment.receive}")
+  boolean isPaymentReceive;
+
   public ResponseEntity<PaymentDto> createOrder(UUID userUuid, CreateOrderDto createOrderDto) {
+    if (!isPaymentReceive) {
+      return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
     var userOpt = userService.findById(userUuid);
     if (userOpt.isEmpty()) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     var user = userOpt.get();
